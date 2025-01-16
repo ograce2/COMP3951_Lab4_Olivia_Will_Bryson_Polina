6472125737bc4f8dc969719224975124724372ae
jasonpeacock25 <jasonpeacock25@yahoo.ca>
2025-01-16 14:09:04 -0800

    Add project files.

---
+++ COMP3951 Lab2 Olivia Grace Jason Peacock/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP3951_Lab2_Olivia_Grace_Jason_Peacock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ COMP3951 Lab2 Olivia Grace Jason Peacock/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP3951_Lab2_Olivia_Grace_Jason_Peacock
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
