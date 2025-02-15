7a735cf3ce01faa846f1ce3d3767f7c7e697c4ea
Bryson Lindy <bslindy@gmail.com>
2025-02-14 19:19:48 -0800

    fixed a string typo

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using COMP3951_Lab02_Olivia_Grace_Jason_Peacock;
+using COMP3951_Lab2_Olivia_Grace_Jason_Peacock;
 using System;
 using System.Diagnostics;
 
@@ -9,16 +9,32 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
-        private Calculator caculator
+        private Calculator calculator;
+
         [TestInitialize]
         public void InitCalc()
         {
-            
+            calculator = new Calculator();
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddition()
         {
+            calculator.setOperand(2);
+            calculator.setOperation("+");
+            calculator.setOperand(8);
+            double? result = calculator.performCalculation();
+            Assert.AreEqual(10, result, "2 + 8 should be 10!");
+        }
+
+        [TestMethod]
+        public void TestSubtraction()
+        {
+            calculator.setOperand(10);
+            calculator.setOperation("-");
+            calculator.setOperand(2);
+            double? result = calculator.performCalculation();
+            Assert.AreEqual(2, result, "10 - 8 should be 2!");
         }
     }
 }
